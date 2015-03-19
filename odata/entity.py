9c8f1cfd29a325e6fff4d88d76ec2dd19a6c3d8c
Tuomas Mursu <tuomas.mursu@kapsi.fi>
2015-03-19 15:26:41 +0200

    Property to separate file

--- odata/entity.py
+++ odata/entity.py
@@ -7,87 +7,7 @@ except ImportError:
     # noinspection PyUnresolvedReferences
     from urlparse import urljoin
 
-
-class Property(object):
-
-    def __init__(self, name, primary_key=False):
-        self.name = name
-        self.primary_key = primary_key
-
-    def __repr__(self):
-        return '<Property {0}>'.format(self.name)
-
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-
-        if self.name in instance.__odata__:
-            return instance.__odata__[self.name]
-        else:
-            raise AttributeError()
-
-    def __set__(self, instance, value):
-        if self.name in instance.__odata__:
-            instance.__odata__[self.name] = value
-
-    def escape_value(self, value):
-        return value
-
-    # Ordering
-
-    def asc(self):
-        return '{0} asc'.format(self.name)
-
-    def desc(self):
-        return '{0} desc'.format(self.name)
-
-    # Comparators supported by OData ###########################################
-
-    def __eq__(self, other):
-        value = self.escape_value(other)
-        return u'{0} eq {1}'.format(self.name, value)
-
-    def __ne__(self, other):
-        value = self.escape_value(other)
-        return u'{0} ne {1}'.format(self.name, value)
-
-    def __ge__(self, other):
-        value = self.escape_value(other)
-        return u'{0} ge {1}'.format(self.name, value)
-
-    def __gt__(self, other):
-        value = self.escape_value(other)
-        return u'{0} gt {1}'.format(self.name, value)
-
-    def __le__(self, other):
-        value = self.escape_value(other)
-        return u'{0} le {1}'.format(self.name, value)
-
-    def __lt__(self, other):
-        value = self.escape_value(other)
-        return u'{0} lt {1}'.format(self.name, value)
-
-    def substringof(self, value):
-        value = self.escape_value(value)
-        return u'substringof({1}, {0})'.format(self.name, value)
-
-    def startswith(self, value):
-        value = self.escape_value(value)
-        return u'startswith({0}, {1})'.format(self.name, value)
-
-    def endswith(self, value):
-        value = self.escape_value(value)
-        return u'endswith({0}, {1})'.format(self.name, value)
-
-
-class IntegerProperty(Property):
-    pass
-
-
-class StringProperty(Property):
-
-    def escape_value(self, value):
-        return u"'{0}'".format(value)
+from .property import PropertyBase, StringProperty, IntegerProperty
 
 
 class EntityBase(object):
@@ -108,7 +28,7 @@ class EntityBase(object):
     def __odata_pk_property__(cls):
         for prop_name in cls.__dict__:
             prop = cls.__dict__.get(prop_name)
-            if isinstance(prop, Property) and prop.primary_key is True:
+            if isinstance(prop, PropertyBase) and prop.primary_key is True:
                 return prop_name, prop
 
     def __odata_instance_url__(self):
@@ -129,7 +49,7 @@ class EntityBase(object):
         else:
             for prop_name in cls.__dict__:
                 prop = cls.__dict__.get(prop_name)
-                if isinstance(prop, Property):
+                if isinstance(prop, PropertyBase):
                     i.__odata__[prop.name] = None
 
         return i
---
+++ odata/property.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Tuomas Mursu <tuomas.mursu@headpower.fi>
+# Copyright © 2015 HeadPower Oy
+
+
+class PropertyBase(object):
+
+    def __init__(self, name, primary_key=False):
+        """
+        :type name: str
+        :type primary_key: bool
+        """
+        self.name = name
+        self.primary_key = primary_key
+
+    def __repr__(self):
+        return '<Property {0}>'.format(self.name)
+
+    def __get__(self, instance, owner):
+        """
+        :type instance: odata.entity.EntityBase
+        :type owner: odata.entity.EntityBase
+        """
+        if instance is None:
+            return self
+
+        if self.name in instance.__odata__:
+            return instance.__odata__[self.name]
+        else:
+            raise AttributeError()
+
+    def __set__(self, instance, value):
+        """
+        :type instance: odata.entity.EntityBase
+        """
+        if self.name in instance.__odata__:
+            instance.__odata__[self.name] = value
+
+    def escape_value(self, value):
+        return value
+
+    def asc(self):
+        return '{0} asc'.format(self.name)
+
+    def desc(self):
+        return '{0} desc'.format(self.name)
+
+    def __eq__(self, other):
+        value = self.escape_value(other)
+        return u'{0} eq {1}'.format(self.name, value)
+
+    def __ne__(self, other):
+        value = self.escape_value(other)
+        return u'{0} ne {1}'.format(self.name, value)
+
+    def __ge__(self, other):
+        value = self.escape_value(other)
+        return u'{0} ge {1}'.format(self.name, value)
+
+    def __gt__(self, other):
+        value = self.escape_value(other)
+        return u'{0} gt {1}'.format(self.name, value)
+
+    def __le__(self, other):
+        value = self.escape_value(other)
+        return u'{0} le {1}'.format(self.name, value)
+
+    def __lt__(self, other):
+        value = self.escape_value(other)
+        return u'{0} lt {1}'.format(self.name, value)
+
+    def startswith(self, value):
+        value = self.escape_value(value)
+        return u'startswith({0}, {1})'.format(self.name, value)
+
+    def endswith(self, value):
+        value = self.escape_value(value)
+        return u'endswith({0}, {1})'.format(self.name, value)
+
+
+class IntegerProperty(PropertyBase):
+    pass
+
+
+class StringProperty(PropertyBase):
+
+    def escape_value(self, value):
+        return u"'{0}'".format(value)
