b3652c6a06b849fec7efda0ff4a7a261e3807c6a
Tuomas Mursu <tuomas.mursu@kapsi.fi>
2015-04-14 13:41:50 +0300

    Test expanded NavigationProperty

--- odata/tests/test_navigation.py
+++ odata/tests/test_navigation.py
@@ -60,6 +60,55 @@ class TestNavigationObjects(unittest.TestCase):
             assert isinstance(part, ProductPart)
 
     @responses.activate
+    def test_read_expanded_navigation_property(self):
+        # Initial data ########################################################
+        def request_callback(request):
+            payload = {
+                'ProductID': 51,
+                'ProductName': 'Foo',
+                'Category': 'Bar',
+                'Price': 12.3,
+                'Parts': [
+                    {
+                        'PartID': 512,
+                        'PartName': 'Bits and bobs',
+                        'Size': 5.333,
+                    }
+                ]
+            }
+
+            resp_body = {'value': [payload]}
+            headers = {}
+            return requests.codes.ok, headers, json.dumps(resp_body)
+
+        responses.add_callback(
+            responses.GET, ProductWithNavigation.__odata_url__(),
+            callback=request_callback,
+            content_type='application/json',
+        )
+        #######################################################################
+
+        query = Service.query(ProductWithNavigation)
+        query.expand(ProductWithNavigation.parts)
+        product = query.first()
+
+        # Get parts ###########################################################
+        parts_url = product.__odata__.instance_url + '/Parts'
+
+        def request_callback_parts(request):
+            assert False, 'Expanded NavigationProperty should not cause GET'
+
+        responses.add_callback(
+            responses.GET, parts_url,
+            callback=request_callback_parts,
+            content_type='application/json',
+        )
+        #######################################################################
+
+        for part in product.parts:
+            assert isinstance(part, ProductPart)
+
+    @responses.activate
     def test_set_navigation_property(self):
         # Initial data ########################################################
         def request_callback(request):
