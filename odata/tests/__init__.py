462b795fde1e8fad4ffeed3c90d911d3c8167e4c
Tuomas Mursu <tuomas.mursu@kapsi.fi>
2015-04-14 12:56:54 +0300

    Some navigation tests

--- odata/tests/__init__.py
+++ odata/tests/__init__.py
@@ -2,7 +2,7 @@
 
 from odata import ODataService
 from odata.entity import declarative_base
-from odata.property import StringProperty, IntegerProperty, DecimalProperty
+from odata.property import StringProperty, IntegerProperty, DecimalProperty, NavigationProperty
 
 Base = declarative_base()
 url = 'http://unittest.server.local/odata/'
@@ -14,3 +14,25 @@ class Product(Base):
     name = StringProperty('ProductName')
     category = StringProperty('Category')
     price = DecimalProperty('Price')
+
+
+class ProductPart(Base):
+    __odata_type__ = 'ODataTest.Objects.ProductPart'
+    __odata_collection__ = 'ProductParts'
+    id = IntegerProperty('PartID', primary_key=True)
+    name = StringProperty('PartName')
+    size = DecimalProperty('Size')
+    product_id = IntegerProperty('ProductID')
+
+
+class ProductWithNavigation(Base):
+    __odata_type__ = 'ODataTest.Objects.ProductWithNavigation'
+    __odata_collection__ = 'ProductsWithNavigation'
+    id = IntegerProperty('ProductID', primary_key=True)
+    name = StringProperty('ProductName')
+    category = StringProperty('Category')
+    price = DecimalProperty('Price')
+
+
+ProductPart.product = NavigationProperty('Product', ProductWithNavigation, foreign_key='ProductID')
+ProductWithNavigation.parts = NavigationProperty('Parts', ProductPart, collection=True)
---
+++ odata/tests/test_navigation.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+import json
+
+import responses
+import requests
+
+from odata.tests import Service, ProductWithNavigation, ProductPart
+
+
+class TestNavigationObjects(unittest.TestCase):
+
+    @responses.activate
+    def test_read_navigation_property(self):
+        # Initial data ########################################################
+        def request_callback(request):
+            payload = {
+                'ProductID': 51,
+                'ProductName': 'Foo',
+                'Category': 'Bar',
+                'Price': 12.3,
+            }
+
+            resp_body = {'value': [payload]}
+            headers = {}
+            return requests.codes.ok, headers, json.dumps(resp_body)
+
+        responses.add_callback(
+            responses.GET, ProductWithNavigation.__odata_url__(),
+            callback=request_callback,
+            content_type='application/json',
+        )
+        #######################################################################
+
+        product = Service.query(ProductWithNavigation).first()
+
+        # Get parts ###########################################################
+        parts_url = product.__odata__.instance_url + '/Parts'
+
+        def request_callback_parts(request):
+            payload = {
+                'PartID': 512,
+                'PartName': 'Bits and bobs',
+                'Size': 5.333,
+            }
+
+            resp_body = {'value': [payload]}
+            headers = {}
+            return requests.codes.ok, headers, json.dumps(resp_body)
+
+        responses.add_callback(
+            responses.GET, parts_url,
+            callback=request_callback_parts,
+            content_type='application/json',
+        )
+        #######################################################################
+
+        for part in product.parts:
+            assert isinstance(part, ProductPart)
+
+    @responses.activate
+    def test_set_navigation_property(self):
+        # Initial data ########################################################
+        def request_callback(request):
+            payload = {
+                'ProductID': 51,
+                'ProductName': 'Foo',
+                'Category': 'Bar',
+                'Price': 12.3,
+            }
+
+            resp_body = {'value': [payload]}
+            headers = {}
+            return requests.codes.ok, headers, json.dumps(resp_body)
+
+        responses.add_callback(
+            responses.GET, ProductWithNavigation.__odata_url__(),
+            callback=request_callback,
+            content_type='application/json',
+        )
+        #######################################################################
+
+        product = Service.query(ProductWithNavigation).first()
+
+        # Get part ############################################################
+        def request_callback_parts(request):
+            payload = {
+                'PartID': 512,
+                'PartName': 'Bits and bobs',
+                'Size': 5.333,
+            }
+
+            resp_body = {'value': [payload]}
+            headers = {}
+            return requests.codes.ok, headers, json.dumps(resp_body)
+
+        responses.add_callback(
+            responses.GET, ProductPart.__odata_url__(),
+            callback=request_callback_parts,
+            content_type='application/json',
+        )
+        #######################################################################
+
+        part = Service.query(ProductPart).first()
+        product.parts = [part]
+
+        # Patch call ##########################################################
+        def request_callback_set_parts(request):
+            payload = json.loads(request.body)
+
+            key = 'Parts@odata.bind'
+            assert key in payload
+            assert payload.get(key) == ['ProductParts(512)']
+
+            headers = {}
+            return requests.codes.no_content, headers, ''
+
+        responses.add_callback(
+            responses.PATCH, product.__odata__.instance_url,
+            callback=request_callback_set_parts,
+            content_type='application/json',
+        )
+        #######################################################################
+
+        # Reload data #########################################################
+        def request_callback_reload(request):
+            payload = {
+                'ProductID': 51,
+                'ProductName': 'Foo',
+                'Category': 'Bar',
+                'Price': 12.3,
+            }
+
+            resp_body = payload
+            headers = {}
+            return requests.codes.ok, headers, json.dumps(resp_body)
+
+        responses.add_callback(
+            responses.GET, product.__odata__.instance_url,
+            callback=request_callback,
+            content_type='application/json',
+        )
+        #######################################################################
+
+        Service.save(product)
