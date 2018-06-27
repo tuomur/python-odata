d894eb604be1dc44be19b8c8f76d2397537e9ae9
Jonathan Harker <jonathan@catalyst.net.nz>
2018-06-27 16:45:05 +1200

    Update setup.py to allow deployments to PyPI - #27

--- setup.py
+++ setup.py
@@ -1,29 +1,5 @@
 # -*- coding: utf-8 -*-
-
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup
 
-requires = [
-    'requests>=2.0',
-    'python-dateutil',
-]
-
-# support for enums from pypi when on older python
-if sys.version_info < (3, 4):
-    requires.append('enum34')
-
-tests_require = (
-    'responses',
-)
-
-setup(
-    name='odata',
-    version='0.3',
-    description='A simple library for read/write access to OData services.',
-    license='MIT',
-    author='Tuomas Mursu',
-    author_email='tuomas.mursu@kapsi.fi',
-    install_requires=requires,
-    tests_require=tests_require,
-    packages=find_packages(),
-)
+setup(pbr=True)
