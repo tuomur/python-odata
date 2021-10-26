8cb30c2f54031d63b43b5e0f23cbed26254c1945
jarra <suhrawardi@gmail.com>
2021-10-26 09:10:57 +0200

    urling

--- odata/state.py
+++ odata/state.py
@@ -120,19 +120,15 @@ class EntityState(object):
                     return self.odata_scope
             elif odata_id and self.id in odata_id:
                 url = re.sub(self.entity.__odata_collection__, '', self.entity.__odata_url__())
-                if odata_id.startswith('http'):
-                    odata_id = urlparse(odata_id).path.split('/')[-1]
-                    return urljoin(url, odata_id)
-                else:
-                    return urljoin(url, odata_id)
+                odata_id = odata_id.split('/')[-1]
+                return urljoin(url, odata_id)
             else:
                 url = re.sub(self.entity.__odata_collection__, '', self.entity.__odata_url__())
                 return urljoin(url, self.id)
-        elif odata_id and odata_id.startswith('http'):
-            odata_id = urlparse(odata_id).path.split('/')[-1]
-            return urljoin(self.entity.__odata_service__.url, odata_id)
         elif odata_id:
-            return urljoin(self.entity.__odata_service__.url, odata_id)
+            url = re.sub(self.entity.__odata_collection__, '', self.entity.__odata_url__())
+            odata_id = odata_id.split('/')[-1]
+            return urljoin(url, odata_id)
 
     @property
     def properties(self):
